45179e9e0d7c1e0017e067f447f72aa9045de7b9
yleonf <yasnaleonfoitzick@gmail.com>
2025-05-19 15:50:52 -0400

    Delete Student

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const students=[];
 
 const tableBody=document.querySelector("#studentsTable tbody");
+const averageDiv=document.getElementById("average");
 
 document.getElementById("studentForm").addEventListener("submit",function(e){
     e.preventDefault();
 
     const name=document.getElementById("name").value.trim();
     const lastName=document.getElementById("lastName").value.trim();
-    const grade=document.getElementById("grade").value.trim();
+    const grade=parseFloat(document.getElementById("grade").value.trim());
 
     if(grade <1 || grade >7 || !name || !lastName || isNaN(grade)){
     alert("Error Datos Incorrectos")
@@ -19,6 +20,7 @@ document.getElementById("studentForm").addEventListener("submit",function(e){
     const student={name,lastName,grade};
     students.push(student);
      addStudentToTable(student)
+     calcularPromedio()
    // console.log(students)
 
     this.reset();
@@ -30,6 +32,33 @@ function addStudentToTable(student){
     row.innerHTML=
     `<td>${student.name}</td>
      <td>${student.lastName}</td>
-    <td>${student.grade}</td>`;
+    <td>${student.grade}</td>
+    <td> <button class="delete">Eliminar</button></td>
+    `;
+ row.querySelector(".delete").addEventListener("click",function(){
+     deleteEstudiante(student,row);
+ });
    tableBody.appendChild(row);
+}
+
+function deleteEstudiante(student,row){
+    // buscar el estudiante en el array
+    const index=students.indexOf(student);
+    if(index > -1){
+        students.splice(index,1);
+        row.remove();
+        calcularPromedio();
+    }
+}
+
+function calcularPromedio(){
+    if(students.length===0){
+       averageDiv.textContent="Promedio General del Curso:N/A"
+        return;
+    }
+    const total=students.reduce((sum,s)=>sum+s.grade,0);
+    console.log(total)
+    const average=total/students.length;
+    console.log(average)
+    averageDiv.textContent=`Promedio General del Curso: ${average.toFixed(2)}`;
 }
